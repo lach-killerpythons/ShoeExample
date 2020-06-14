7d23652049f79f85fed92d4992c7d6e05d489cca
lach-killerpythons <lachlanmcbride5@gmail.com>
2020-06-14 12:00:30 +1000

    first commit

---
+++ src/ShoeExample.java
@@ -0,0 +1,43 @@
+public class ShoeExample {
+    abstract class Shoe {
+        public final String myBrand;
+        public final double mySize;
+        Shoe(String brand, double size){
+            this.myBrand=brand;
+            this.mySize=size;
+        }
+    }
+    class Walking extends Shoe {
+        public final Boolean goreTex;
+        public Walking(String brand, double size,Boolean goreTex){
+            super(brand,size);
+            this.goreTex=goreTex;
+        }
+    }
+
+    class Running extends Shoe {
+        public final double weight;
+        public Running(String brand, double size,double weight){
+            super(brand,size);
+            this.weight=weight;
+        }
+    }
+
+    public static void main(String[] args){
+        ShoeExample S1 = new ShoeExample();
+        ShoeExample.Shoe w1 = S1.new Walking("Merrell",42,false);
+        ShoeExample.Shoe w2 = S1.new Walking("Hanwag",43.5,true);
+        ShoeExample.Shoe r1 = S1.new Running("Nike",43,250);
+        Shoe[] shoeStore = {w1,w2,r1};
+        for(Shoe S:shoeStore){
+            System.out.print(S.myBrand + "\t" + S.mySize + "\t");
+            if (S instanceof Running){
+                System.out.print("weight: "+((Running) S).weight);
+            }
+            if (S instanceof Walking){
+                System.out.print("gortex: "+((Walking) S).goreTex);
+            }
+            System.out.println();
+        }
+    }
+}
